608c1a796bb1c741c711604eb609eb55599a6cad
Tsuna21182 <tsunadelanochecer@gmail.com>
2025-01-29 15:46:23 -0500

    primera entrada

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="flex justify-center items-center h-dvh">
+      <section className="bg-White p-5 rounded-2xl">
+        <div className="flex gap-4 items-center">
+          <img src="/images/icon-star.svg" alt="" />
+          <h1 className="text-5xl text-Dark-purple font-bold">FAQs</h1>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
